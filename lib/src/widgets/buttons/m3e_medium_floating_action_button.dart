8edefa746a1e86d5abaad3e785c42d705fdbc2b5
Guillaume Roux <rouxguillaume8@gmail.com>
2025-06-15 12:34:34 +0200

    feat: add template annotation to comments

--- lib/src/widgets/buttons/m3e_medium_floating_action_button.dart
+++ lib/src/widgets/buttons/m3e_medium_floating_action_button.dart
@@ -47,6 +47,7 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   /// {@endtemplate}
   final String? tooltip;
 
+  /// {@template material_expressive.fab.foreground_color}
   /// The default foreground color for icons and text within the button.
   ///
   /// If this property is null, then the [FloatingActionButtonThemeData.foregroundColor]
@@ -54,8 +55,10 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   /// null, then the [ColorScheme.onPrimaryContainer] color of [ThemeData.colorScheme]
   /// is used. If [ThemeData.useMaterial3] is set to false, then the
   /// [ColorScheme.onSecondary] color of [ThemeData.colorScheme] is used.
+  /// {@endtemplate}
   final Color? foregroundColor;
 
+  /// {@template material_expressive.fab.background_color}
   /// The button's background color.
   ///
   /// If this property is null, then the [FloatingActionButtonThemeData.backgroundColor]
@@ -63,28 +66,36 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   /// null, then the [ColorScheme.primaryContainer] color of [ThemeData.colorScheme]
   /// is used. If [ThemeData.useMaterial3] is set to false, then the
   /// [ColorScheme.secondary] color of [ThemeData.colorScheme] is used.
+  /// {@endtemplate}
   final Color? backgroundColor;
 
+  /// {@template material_expressive.fab.focus_color}
   /// The color to use for filling the button when the button has input focus.
   ///
   /// In Material 3, defaults to [ColorScheme.onPrimaryContainer] with opacity 0.1.
   /// In Material 2, it defaults to [ThemeData.focusColor] for the current theme.
+  /// {@endtemplate}
   final Color? focusColor;
 
+  /// {@template material_expressive.fab.hover_color}
   /// The color to use for filling the button when the button has a pointer
   /// hovering over it.
   ///
   /// Defaults to [ThemeData.hoverColor] for the current theme in Material 2. In
   /// Material 3, defaults to [ColorScheme.onPrimaryContainer] with opacity 0.08.
+  /// {@endtemplate}
   final Color? hoverColor;
 
+  /// {@template material_expressive.fab.splash_color}
   /// The splash color for this [M3EMediumFloatingActionButton]'s [InkWell].
   ///
   /// If null, [FloatingActionButtonThemeData.splashColor] is used, if that is
   /// null, [ThemeData.splashColor] is used in Material 2; [ColorScheme.onPrimaryContainer]
   /// with opacity 0.1 is used in Material 3.
+  /// {@endtemplate}
   final Color? splashColor;
 
+  /// {@template material_expressive.fab.hero_tag}
   /// The tag to apply to the button's [Hero] widget.
   ///
   /// Defaults to a tag that matches other floating action buttons.
@@ -97,18 +108,24 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   /// there would be a tag conflict (multiple heroes on one route can't have the
   /// same tag). The Material Design specification recommends only using one
   /// floating action button per screen.
+  /// {@endtemplate}
   final Object? heroTag;
 
+  /// {@template material_expressive.fab.on_pressed}
   /// The callback that is called when the button is tapped or otherwise activated.
   ///
   /// If this is set to null, the button will be disabled.
+  /// {@endtemplate}
   final VoidCallback? onPressed;
 
+  /// {@template material_expressive.fab.mouse_cursor}
   /// {@macro flutter.material.RawMaterialButton.mouseCursor}
   ///
   /// If this property is null, [WidgetStateMouseCursor.clickable] will be used.
+  /// {@endtemplate}
   final MouseCursor? mouseCursor;
 
+  /// {@template material_expressive.fab.elevation}
   /// The z-coordinate at which to place this button relative to its parent.
   ///
   /// This controls the size of the shadow below the floating action button.
@@ -120,8 +137,10 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   ///
   ///  * [highlightElevation], the elevation when the button is pressed.
   ///  * [disabledElevation], the elevation when the button is disabled.
+  /// {@endtemplate}
   final double? elevation;
 
+  /// {@template material_expressive.fab.focus_elevation}
   /// The z-coordinate at which to place this button relative to its parent when
   /// the button has the input focus.
   ///
@@ -135,8 +154,10 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   ///  * [elevation], the default elevation.
   ///  * [highlightElevation], the elevation when the button is pressed.
   ///  * [disabledElevation], the elevation when the button is disabled.
+  /// {@endtemplate}
   final double? focusElevation;
 
+  /// {@template material_expressive.fab.hover_elevation}
   /// The z-coordinate at which to place this button relative to its parent when
   /// the button is enabled and has a pointer hovering over it.
   ///
@@ -150,8 +171,10 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   ///  * [elevation], the default elevation.
   ///  * [highlightElevation], the elevation when the button is pressed.
   ///  * [disabledElevation], the elevation when the button is disabled.
+  /// {@endtemplate}
   final double? hoverElevation;
 
+  /// {@template material_expressive.fab.highlight_elevation}
   /// The z-coordinate at which to place this button relative to its parent when
   /// the user is touching the button.
   ///
@@ -163,8 +186,10 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   /// See also:
   ///
   ///  * [elevation], the default elevation.
+  /// {@endtemplate}
   final double? highlightElevation;
 
+  /// {@template material_expressive.fab.disabled_elevation}
   /// The z-coordinate at which to place this button when the button is disabled
   /// ([onPressed] is null).
   ///
@@ -178,26 +203,36 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   ///
   ///  * [elevation], the default elevation.
   ///  * [highlightElevation], the elevation when the button is pressed.
+  /// {@endtemplate}
   final double? disabledElevation;
 
+  /// {@template material_expressive.fab.shape}
   /// The shape of the button's [Material].
   ///
   /// The button's highlight and splash are clipped to this shape. If the
   /// button has an elevation, then its drop shadow is defined by this
   /// shape as well.
+  /// {@endtemplate}
   final ShapeBorder? shape;
 
+  /// {@template material_expressive.fab.clip_behavior}
   /// {@macro flutter.material.Material.clipBehavior}
   ///
   /// Defaults to [Clip.none].
+  /// {@endtemplate}
   final Clip clipBehavior;
 
+  /// {@template material_expressive.fab.focus_node}
   /// {@macro flutter.widgets.Focus.focusNode}
+  /// {@endtemplate}
   final FocusNode? focusNode;
 
+  /// {@template material_expressive.fab.autofocus}
   /// {@macro flutter.widgets.Focus.autofocus}
+  /// {@endtemplate}
   final bool autofocus;
 
+  /// {@template material_expressive.fab.material_tap_target_size}
   /// Configures the minimum size of the tap target.
   ///
   /// Defaults to [ThemeData.materialTapTargetSize].
@@ -205,8 +240,10 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   /// See also:
   ///
   ///  * [MaterialTapTargetSize], for a description of how this affects tap targets.
+  /// {@endtemplate}
   final MaterialTapTargetSize? materialTapTargetSize;
 
+  /// {@template material_expressive.fab.enable_feedback}
   /// Whether detected gestures should provide acoustic and/or haptic feedback.
   ///
   /// For example, on Android a tap will produce a clicking sound and a
@@ -218,6 +255,7 @@ class M3EMediumFloatingActionButton extends StatelessWidget {
   /// See also:
   ///
   ///  * [Feedback] for providing platform-specific feedback to certain actions.
+  /// {@endtemplate}
   final bool? enableFeedback;
 
   @override
